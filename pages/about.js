e85b91cfec9364317706558f168cd1fe38e000f7
asif khokhar <117714415+asifkhokhar11@users.noreply.github.com>
2023-01-23 13:16:27 +0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+const about = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default about
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
